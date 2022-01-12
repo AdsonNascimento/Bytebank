7d8edab77396cffa7c899d73b671de37a104b302
AdsonNascimento <adson.nascimento.112@ufrn.edu.br>
2022-01-12 10:24:23 -0300

    update

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransfeir = new EventEmitter<any>();
 
+  valor: number;
+  destino: number;
+
+  transferir() {
+    //confirma button pres
+    console.log('Solicitada nova tranferência');
+
+    //atribui valores
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransfeir.emit(valorEmitir);
+
+    //limpa campos
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
