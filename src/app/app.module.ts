b71c76e239a31579853dfcc2f6d0b0f9ca68ffd6
AdsonNascimento <adson.nascimento.112@ufrn.edu.br>
2022-01-12 07:21:26 -0300

    new style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule
